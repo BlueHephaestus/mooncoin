4d265734bea7522d1f592404c664a31903b5a4a3
blue <bluehephaestus@gmail.com>
2024-10-16 21:21:31 -0700

    Updated everything for launch, set up updater script!

--- main.py
+++ main.py
@@ -1,70 +1,32 @@
-import requests
 import json
+from datetime import datetime
+import shutil
 
-from DONTSHARE import *
 
+# dst_link = "https://raw.githubusercontent.com/BlueHephaestus/mooncoin/refs/heads/main/moon.png"
+imgs_dir = "imgs/"
+dst_file = "moon.png"
+log_file = "updates.log"
 
-# link = "https://raw.githubusercontent.com/BlueHephaestus/mooncoin/refs/heads/main/000.png?token=GHSAT0AAAAAACVFSDN4VNEJCFC3KKPHGD62ZX5UC2Q"
+# Get correct image from lookup for tonight
+# Copy it to the correct location
 
-# link = "https://github.com/BlueHephaestus/mooncoin/blob/main/000.png?raw=true"
-link = "https://raw.githubusercontent.com/BlueHephaestus/mooncoin/refs/heads/main/000.png"
-def update_token(token_addr, icon_fname, banner_fname, desc):
-    url = f"https://ms.dexscreener.com/tokens/v1/solana/{token_addr}"
-
-    payload = {
-        'chainId': 'solana',
-        'tokenAddress': '5fQEuytDt21sotZeekRyYGgK1oGZVdmF9yAHfSq4s1hQ',
-        'description': "",
-        'signature': SIGNATURE,
-        'links': '[]',
-    }
-    # 'icon': link,
-    files = []
-    # files = [
-    #     ('icon', ('2.png', open('2.png', 'rb'), 'image/png')),
-    # ]
-    # ('banner', ('2.png', open('2.png', 'rb'), 'image/png'))
-
-    # files = [
-    #     ('icon', (link, open('test.php.png','rb'), 'image/png')),
-    # ]
-    # files = {
-    #     'icon': {"id": "myowntextfuckyouidowhatiwant"},
-    # }
-    # files = {
-    #     'icon': link.encode("utf-8")
-    # }
-    # files.append(('icon', (icon_fname, link, 'image/*')))
-    # files.append(('icon', (icon_fname, open(icon_fname, 'rb'), 'image/png')))
-    files.append(('icon', (icon_fname, open("2.png", 'rb'), 'image/png')))
-    "*.jpeg, *.jpg, *.png, *.webp, *.gif"
-    #
-    # if banner_fname is not None:
-    #     files.append(('banner', (banner_fname, open(banner_fname, 'rb'), 'image/png')))
-
-    headers = {
-        'authority': 'ms.dexscreener.com',
-        'accept': '*/*',
-        'accept-language': 'en-US,en;q=0.9',
-        'dnt': '1',
-        'origin': 'https://dexscreener.com',
-        'referer': 'https://dexscreener.com/',
-        'sec-ch-ua': '"Not(A:Brand";v="24", "Chromium";v="122"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"Linux"',
-        'sec-fetch-dest': 'empty',
-        'sec-fetch-mode': 'cors',
-        'sec-fetch-site': 'same-site',
-        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'
-    }
-
-    response = requests.request("POST", url, headers=headers, data=payload, files=files)
-
-    print(response.content)
-    if len(response.content):
-        print(response.json())
-    print(response.status_code)
+# Get current date
+today = datetime.now().strftime("%Y-%m-%d")
 
 with open("lookup.json", "r") as f:
     lookup = json.load(f)
-update_token("5fQEuytDt21sotZeekRyYGgK1oGZVdmF9yAHfSq4s1hQ", "2.png", "2.png", "test desc")
+
+# Get moon data for today
+moon_data = lookup[today]
+img_fname, _, desc = moon_data
+img_fpath = f"{imgs_dir}{img_fname}"
+
+# Copy to dst file.
+shutil.copyfile(img_fpath, dst_file)
+
+# Update log file.
+with open(log_file, "a") as f:
+    f.write(f"{today}: Icon: {img_fname}, Desc: {desc}\n")
+
+print(f"Updated {dst_file} with {img_fname} for {today}")
---
+++ update.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Go to your project directory (where the repo is located)
+MOON_DIR=/home/blue/Projects/mooncoin
+
+cd $MOON_DIR
+
+# Update the file
+python3.11 main.py
+
+# Stage the file
+git add moon.png
+
+# Commit the file with a message
+git commit -m "Auto update $(date)"
+
+# Push to the GitHub repository
+git push origin main
